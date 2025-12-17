6febb97e89f595a51370d378992e9a2dd7d98d8d
putrinur atisyah <putrinuratisyah@gmail.com>
2025-12-18 02:42:51 +0700

    Praktikum G: anonymous class

--- Anonymous.php
+++ Anonymous.php
@@ -1,13 +1,21 @@
 <?php
-// Class Anonymous dengan constructor dan property promotion
+// Kode Asli
 $logger = new class("INFO") {
-    public function __construct(public string $level) {}
+    public function __construct(public string $level) {} // <-- Properti $level
+    public function log(string $msg) { echo "[{$this->level}] {$msg}" . PHP_EOL; }
+};
+
+// =========================================
+// MODIFIKASI: Menambahkan properti $tag
+// =========================================
+$logger_modifikasi = new class("INFO", "MAIN") { 
+    // Properti baru $tag ditambahkan di sini menggunakan Property Promotion
+    public function __construct(public string $level, public string $tag) {} 
 
     public function log(string $msg) { 
-        echo "[{$this->level}] {$msg}" . PHP_EOL; 
+        echo "[{$this->level}] [{$this->tag}] {$msg}" . PHP_EOL; 
     }
 };
 
-$logger->log("Sistem berjalan");
-$logger->level = "DEBUG"; // Bisa diubah karena public
-$logger->log("Data diproses");
+$logger_modifikasi->log("Sistem berjalan");
+// Output: [INFO] [MAIN] Sistem berjalan
